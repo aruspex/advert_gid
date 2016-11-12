e85cf35d2ea655ac50955a694a479cd48e2c67f0
Anton <arspex@gmail.com>
2016-11-12 09:25:54 +0200

    add users(with hobbies) page

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -6,5 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminController extends Controller
 {
+    public function usersAction()
+    {
+        $users = $this->getDoctrine()
+            ->getRepository('AppBundle:User')->findAll();
+
+        return $this->render('app/users.html.twig', array('users' => $users));
+    }
 
 }
