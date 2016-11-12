a7678e52073c0851e35df0ed5a829f04cd7f82e6
Anton <arspex@gmail.com>
2016-11-12 09:41:34 +0200

    add hobby users page

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -14,4 +14,17 @@ class AdminController extends Controller
         return $this->render('app/users.html.twig', array('users' => $users));
     }
 
+    public function displayHobbyUsersAction($hobbyName)
+    {
+        $hobby = $this->getDoctrine()
+            ->getRepository('AppBundle:Hobby')
+            ->findOneBy(array('name' => $hobbyName));
+
+        if (!$hobby) {
+            throw $this->createNotFoundException($hobbyName . ' hobby does not exist!');
+        }
+
+        return $this->render(':app:hobby_users.html.twig', array('hobby' => $hobby));
+
+    }
 }
